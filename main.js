f8ed2df96707dd2e081fd8fef898515a8cd10451
Ditoo1 <156104433+Ditoo1@users.noreply.github.com>
2024-01-28 12:59:15 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const tabs = document.querySelectorAll(".scrollable-tabs-container a");
     const platosSections = document.querySelectorAll(".platos > div");
+    const seccionComida = document.querySelector(".seccion-comida");
+    const seccionBebidas = document.querySelector(".seccion-bebidas");
+    const seccionTragos = document.querySelector(".seccion-tragos");
 
     const removeAllActiveClasses = () => {
         tabs.forEach((tab) => {
@@ -14,11 +17,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     };
 
-    // Ocultar todas las secciones de platos al inicio
+    const hideAllSecciones = () => {
+        hideAllPlatosSections();
+        seccionComida.style.display = "none";
+        seccionBebidas.style.display = "none";
+        seccionTragos.style.display = "none";
+    };
+
+    // Ocultar todas las secciones al inicio
     hideAllPlatosSections();
+    hideAllSecciones();
 
     // Mostrar la sección de platos fríos por defecto
     platosSections[0].style.display = "block";
+
+    // Simular clic en la pestaña de comida al cargar la página
+    tabs[0].classList.add("active");
+    seccionComida.style.display = "block";
 
     tabs.forEach((tab, index) => {
         tab.addEventListener("click", (event) => {
@@ -29,5 +44,30 @@ document.addEventListener("DOMContentLoaded", () => {
             tab.classList.add("active");
             platosSections[index].style.display = "block";
         });
+    });
+
+    // Agregar event listener para el cambio del switch
+    const switchField = document.querySelector(".switch-field");
+    switchField.addEventListener("change", (event) => {
+        const selectedValue = event.target.value;
+        hideAllSecciones();
+        removeAllActiveClasses();
+
+        switch (selectedValue) {
+            case "platos":
+                seccionComida.style.display = "block";
+                tabs[0].classList.add("active");
+                platosSections[0].style.display = "block";
+                break;
+            case "bebidas":
+                seccionBebidas.style.display = "block";
+                break;
+            case "tragos":
+                seccionTragos.style.display = "block";
+                break;
+            default:
+                // Manejar cualquier otro caso según sea necesario
+                break;
+        }
     });
 });
